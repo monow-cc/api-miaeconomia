e7678040c7c367f6908b850f608a73c4956634db
LuanRodriguesDEV <97750971+LuanRodriguesDEV@users.noreply.github.com>
2023-04-29 14:06:23 -0300

    Criando-Projeto

---
+++ Api-Mercado/Context/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Api_Mercado.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+     
+    }
+}
